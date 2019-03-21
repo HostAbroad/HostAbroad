7f51a4ad17f6b451ed4c46f1fa250b770d912003
evivar <42641415+evivar@users.noreply.github.com>
2019-03-21 10:12:26 +0100

    CommandSearchTraveler

---
+++ HostAbroad/src/main/java/com/presentation/commands/CommandEnum.java
@@ -0,0 +1,10 @@
+package com.presentation.commands;
+
+public class CommandEnum {
+
+	//Clase enum para hacer seleccion dinámica de comando con todos los que usaremos 
+	public enum Commands {
+	    CommandSearchHost,
+	    CommandSearchTraveler
+	}
+}
