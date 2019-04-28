77c09169df3f6f2763610f05e3508a275f8e1b27
Roberto <omegaskyres9@gmail.com>
2019-04-28 23:56:08 +0200

    CommitR

---
+++ HostAbroad/src/main/java/com/business/enums/LanguagesTokens.java
@@ -0,0 +1,72 @@
+package com.business.enums;
+
+import com.fo0.advancedtokenfield.model.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguagesTokens {
+
+        List<Token> tokens;
+
+        public LanguagesTokens() {
+            tokens = new ArrayList<>();
+            tokens.add(new Token("Albanian"));
+            tokens.add(new Token("Arabic"));
+            tokens.add(new Token("Basque"));
+            tokens.add(new Token("Bosnian"));
+            tokens.add(new Token("Bulgarian"));
+            tokens.add(new Token("Catalan"));
+            tokens.add(new Token("Chinese"));
+            tokens.add(new Token("Croatian"));
+            tokens.add(new Token("Czech"));
+            tokens.add(new Token("Danish"));
+            tokens.add(new Token("Dutch"));
+            tokens.add(new Token("English"));
+            tokens.add(new Token("Estonian"));
+            tokens.add(new Token("Faroese"));
+            tokens.add(new Token("Finnish"));
+            tokens.add(new Token("French"));
+            tokens.add(new Token("Frisian"));
+            tokens.add(new Token("Galician"));
+            tokens.add(new Token("German"));
+            tokens.add(new Token("Greek"));
+            tokens.add(new Token("Greenlandic Inuktitut"));
+            tokens.add(new Token("Hindi"));
+            tokens.add(new Token("Hungarian"));
+            tokens.add(new Token("Icelandic"));
+            tokens.add(new Token("Irish"));
+            tokens.add(new Token("Italian"));
+            tokens.add(new Token("Japanese"));
+            tokens.add(new Token("Latin"));
+            tokens.add(new Token("Latvian"));
+            tokens.add(new Token("Lithuanian"));
+            tokens.add(new Token("Luxembourgish"));
+            tokens.add(new Token("Macedonian"));
+            tokens.add(new Token("Maltese"));
+            tokens.add(new Token("Moldovan"));
+            tokens.add(new Token("Norwegian"));
+            tokens.add(new Token("Polish"));
+            tokens.add(new Token("Portuguese"));
+            tokens.add(new Token("Romanian"));
+            tokens.add(new Token("Romansch"));
+            tokens.add(new Token("Russian"));
+            tokens.add(new Token("Serbian"));
+            tokens.add(new Token("Serbo-Croatian"));
+            tokens.add(new Token("Slovak"));
+            tokens.add(new Token("Slovene"));
+            tokens.add(new Token("Slovenian"));
+            tokens.add(new Token("Spanish"));
+            tokens.add(new Token("Swedish"));
+            tokens.add(new Token("Turkish"));
+            tokens.add(new Token("Ukrainian"));
+
+}
+
+        public List<Token> getTokens() {
+            return tokens;
+        }
+
+    }
+
+
